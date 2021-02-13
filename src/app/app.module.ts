6e4e905e2c054262409bb1653e044cd678ce8497
poran200 <chowdury659@gmail.com>
2021-02-14 01:54:33 +0600

    add README.md

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,51 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { LoginComponent } from './pages/login/login.component';
+import { BannerComponent } from './components/banner/banner.component';
+import {MatCardModule} from "@angular/material/card";
+import {ApiService} from "./services/api.service";
+import {HttpClientModule} from "@angular/common/http";
+import { ProductsComponent } from './components/products/products.component';
+import { ChekoutPorductsComponent } from './components/chekout-porducts/chekout-porducts.component';
+import { ChekoutSubtotalComponent } from './components/chekout-subtotal/chekout-subtotal.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatPseudoCheckboxModule} from "@angular/material/core";
+import {AngularFireModule} from "@angular/fire";
+import {firebaseConfig} from "./config/firebaseConfig";
+import {CommonModule} from "@angular/common";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    CheckoutComponent,
+    LoginComponent,
+    BannerComponent,
+    ProductsComponent,
+    ChekoutPorductsComponent,
+    ChekoutSubtotalComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatPseudoCheckboxModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/config/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD61yRQKQnRfCgebOBZ13YxorLlD_joWOk",
+  authDomain: "clone-angular-826f6.firebaseapp.com",
+  projectId: "clone-angular-826f6",
+  storageBucket: "clone-angular-826f6.appspot.com",
+  messagingSenderId: "1066607754065",
+  appId: "1:1066607754065:web:f2ba968474d8f09666f170"
+};
